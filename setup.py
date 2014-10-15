f1011076290bf069d5c5627a5b5cb98d3dd57544
Jaume Bonet <jaume.bonet@gmail.com>
2014-10-15 19:42:46 +0200

    reorganisation of files to meet docs and setup standards

--- setup.py
+++ setup.py
@@ -1,10 +1,64 @@
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from os import path
 
-setup(name='Pynion',
-      version='0.0.1',
-      description='Python Minion Library',
-      author='Jaume Bonet',
-      author_email='jaume.bonet@gmail.com',
-      url='https://bitbucket.org/jaumebonet/pynion',
-      packages=['pynion'],
-      )
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='pynion',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.0.1',
+
+    description='Python Minion Library',
+
+    # The project's main homepage.
+    url='https://bitbucket.org/jaumebonet/pynion',
+
+    # Author details
+    author='Jaume Bonet',
+    author_email='jaume.bonet@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='development',
+
+    install_requires=['pathlib', 'jsonpickle'],
+
+    packages=find_packages(exclude=['docs', 'test']),
+
+    package_data={
+        'pynion': ['config/*'],
+    },
+
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'pynion=pynion:main',
+        ],
+    },
+)
