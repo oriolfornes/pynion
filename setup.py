f4ed460595d9fb05e1cf965bb5303a19863bc0d1
Jaume Bonet <jaume.bonet@gmail.com>
2014-10-15 19:01:37 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Pynion',
+      version='0.0.1',
+      description='Python Minion Library',
+      author='Jaume Bonet',
+      author_email='jaume.bonet@gmail.com',
+      url='https://bitbucket.org/jaumebonet/pynion',
+      packages=['pynion'],
+      )
